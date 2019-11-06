de9626fa9b6b97d1ad66a455f76a6053b42a2dd9
Xandarel <aobordug@gmail.com>
2019-11-06 17:25:07 +0500

    поставлены цели на бедещее

--- MainApp/CriptoClass/ReverseVeshener.cs
+++ MainApp/CriptoClass/ReverseVeshener.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Criptoclass;
 
 namespace CriptoClass
 {
-    public class ReverseVeshener
+    public class ReverseVeshener : Interfase_criptoelements<WordAndKey<string>>
     {
+        WordAndKey<string> element{get;set;} //TODO: проверить как работает свойство
+        public string Code()//TODO: реализовать
+        {
+            throw new NotImplementedException();
+        }
 
+        public string Decode(WordAndKey<string> element)//TODO: реализовать
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ MainApp/CriptoClass/WordAndKey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriptoClass
+{
+    class WordAndKey<T>
+    {
+        string word;
+        T key;
+    }
+}
