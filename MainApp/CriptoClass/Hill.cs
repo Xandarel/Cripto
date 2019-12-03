c1a0b9a4cd0d95231b1492c0899d6ce547541e33
Xandarel <aobordug@gmail.com>
2019-12-03 18:52:00 +0500

    шифрование Хилла работает правильно

--- MainApp/CriptoClass/Hill.cs
+++ MainApp/CriptoClass/Hill.cs
@@ -7,29 +7,30 @@ using Criptoclass;
 
 namespace CriptoClass
 {
-    class Hill : Interfase_criptoelements<List<Matrix<int>>>
+    public class Hill : Interfase_criptoelements<List<Matrix<double>>>
     {
-        public string Code(WordAndKey<List<Matrix<int>>> element)
+        public string Code(WordAndKey<List<Matrix<double>>> element)
         {
             var numberLitera = Converter.ConvertWordToCode(element.Word);
             var n = element.Key[0].ToArray().GetLength(0);
             var start = 0;
-            for (var i=n; i<numberLitera.Length;i+=n)
+            for (var i=n; i<=numberLitera.Length;i+=n)
             {
-                if (i > numberLitera.Length)
+                if (i >= numberLitera.Length)
                     i = numberLitera.Length;
-                var buff=new int[i - start,1];
+                var buff=new double[i - start,1];
                 for (var j = 0; j < i - start; j++)//разделение массива на блоки длинны n
                     buff[j,0] = numberLitera[start+j];
-                var matrix = Matrix<int>.Build.DenseOfArray(buff);
-                matrix = (matrix + element.Key[1]) * element.Key[0];
+                var matrix = Matrix<double>.Build.DenseOfArray(buff);
+                matrix = element.Key[0] * matrix + element.Key[1];
                 for (var t=0;t<matrix.RowCount;t++)
-                    element.Encoded= element.Encoded = Convert.ToString(FindValue.Findvalue(matrix[t,0]) % Languege.z);
+                    element.Encoded = Convert.ToString(FindValue.Findvalue(Convert.ToInt32(matrix[t,0] % Languege.z)));
+                start = i;
             }
             return element.Encoded;//TODO: проверить, работает ли правильно
         }
 
-        public string Decode(WordAndKey<List<Matrix<int>>> element)
+        public string Decode(WordAndKey<List<Matrix<double>>> element)
         {
             throw new NotImplementedException();
         }
--- MainApp/MainApp/Program.cs
+++ MainApp/MainApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Criptoclass;
 using CriptoClass;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 namespace MainApp
 {
     class Program
@@ -8,12 +11,15 @@ namespace MainApp
         static void Main()
         {
             Languege.Libra("eng");
-            var libra = new WordAndKey<int[,]>("FOOL", new int[,] { 
-                                                                    { 2, 1, 2 },
-                                                                    { 0, 1, 2 }
-            });
-            var tr = new Transposition();
+            var key = new List<Matrix<double>>();
+            key.Add(Matrix<double>.Build.DenseOfArray(new double[,] { { 3, 4 }, { 3, 7 } }));
+            key.Add(Matrix<double>.Build.DenseOfArray(new double[,] { {5}, {7} }));
+            Console.WriteLine(key[0]);
+            Console.WriteLine(key[1]);
+            var libra = new WordAndKey<List<Matrix<double>>>("TOWN",key);
+            var tr = new Hill();
             tr.Code(libra);
+            Console.WriteLine(libra.Encoded);
         }
     }
 }
